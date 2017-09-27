52950c0df46a904b6a236a3d838033121c1978fb
Rodrigo <rbustamantejelvez@gmail.com>
2017-09-27 11:52:24 -0300

    Added create-evaluation form | se me fue crear rama :c

---
+++ src/app/components/create-evaluation/create-evaluation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-evaluation',
+  templateUrl: './create-evaluation.component.html',
+  styleUrls: ['./create-evaluation.component.css']
+})
+export class CreateEvaluationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
