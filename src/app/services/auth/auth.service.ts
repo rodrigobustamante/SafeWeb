3420af11aef87bffdef107b68233945b55675cc5
Rodrigo <rbustamantejelvez@gmail.com>
2017-10-04 12:00:08 -0300

    Added reload when login is successful

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
-import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
-  jwtHelper: JwtHelper = new JwtHelper();
   user: any;
   url:  String;
   constructor(private http: Http) {
@@ -19,13 +17,11 @@ export class AuthService {
   }
   storeUser(user){
     localStorage.setItem('user', JSON.stringify(user))
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ')
     this.user = user
+    location.reload()
   }
 
-  loggedIn() {
-    return tokenNotExpired(); 
-  }
+  
 
   logout(){
     this.user = null;
