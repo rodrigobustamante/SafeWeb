90bfe4517cf993019b9da4e7fcbab10660faf229
rodrigobustamante <rbustamantejelvez@gmail.com>
2017-10-09 01:14:29 -0300

    Refactory on folders and added evaluation index

---
+++ src/app/components/evaluation/evaluation-list/evaluation-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs/Rx";
+import "rxjs/add/operator/map";
+
+class Evaluation{
+  id: number;
+  date: any;
+  observation: string;
+  type: string;
+}
+
+@Component({
+  selector: 'app-evaluation-list',
+  templateUrl: './evaluation-list.component.html',
+  styleUrls: ['./evaluation-list.component.css']
+})
+export class EvaluationListComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+  evaluations: Evaluation[] = [];
+  dtTrigger: Subject<any> = new Subject<any>();
+  headers: any;
+  constructor(public http: HttpClient) { }
+
+  ngOnInit() {
+    this.dtOptions = {
+      pagingType: "full_numbers",
+      pageLength: 20,
+      language: {
+        processing: "Procesando...",
+        lengthMenu: "Mostrar _MENU_ registros",
+        zeroRecords: "No se encontraron resultados",
+        emptyTable: "Ningún dato disponible en esta tabla",
+        info:
+          "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+        infoFiltered: "(filtrado de un total de _MAX_ registros)",
+        infoPostFix: "",
+        search: "Buscar:",
+        url: "",
+        loadingRecords: "Cargando...",
+        paginate: {
+          first: "Primero",
+          last: "Último",
+          next: "Siguiente",
+          previous: "Anterior"
+        },
+        aria: {
+          sortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sortDescending:
+            ": Activar para ordenar la columna de manera descendente"
+        }
+      }
+    };
+    this.http.get("http://localhost:4567/evaluations").subscribe(data => {
+      this.evaluations = data["data"];
+      console.log(this.evaluations);
+      this.dtTrigger.next();
+    });
+  }
+}
