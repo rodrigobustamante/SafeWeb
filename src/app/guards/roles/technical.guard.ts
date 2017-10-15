ca552e5b2ea81cc0cf99656da265bb2963efebd1
Rodrigo <rbustamantejelvez@gmail.com>
2017-10-15 15:55:15 -0300

    Added technical guard

--- src/app/guards/roles/technical.guard.ts
+++ src/app/guards/roles/technical.guard.ts
@@ -7,6 +7,8 @@ export class TechnicalGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+      let user = JSON.parse(localStorage.getItem('user').toString())
+      if(user.role.name === "Tecnico") return true;
+      return false;
   }
 }
