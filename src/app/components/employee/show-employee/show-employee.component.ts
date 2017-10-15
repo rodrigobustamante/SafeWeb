a313ee2bbe479a9c84119e2262054c2a187b1806
Rodrigo <rbustamantejelvez@gmail.com>
2017-10-15 15:10:35 -0300

    Added show employee view

--- src/app/components/employee/show-employee/show-employee.component.ts
+++ src/app/components/employee/show-employee/show-employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-show-employee',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-employee.component.css']
 })
 export class ShowEmployeeComponent implements OnInit {
-
-  constructor() { }
+  employees: any;
+  employee:  any;
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log(id)
+    this.http.get(environment.url + "/employees").subscribe(data => {
+      this.employees = data["data"];
+      this.employee = _.find(this.employees, (c) => {
+        return c.id === Number(id)
+      })
+      console.log(this.employee);
+    })
   }
-
 }
