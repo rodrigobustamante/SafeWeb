f2911641bb9ec5349e641f3aa99add5163e48a35
Rodrigo <rbustamantejelvez@gmail.com>
2017-10-24 23:53:52 -0300

    Added table on medical-visit view

--- src/app/components/doctor/medical-visit/medical-visit.component.ts
+++ src/app/components/doctor/medical-visit/medical-visit.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import * as _ from "lodash";
+import { environment } from "./../../../../environments/environment";
+
 
 @Component({
-  selector: 'app-medical-visit',
-  templateUrl: './medical-visit.component.html',
-  styleUrls: ['./medical-visit.component.css']
+  selector: "app-medical-visit",
+  templateUrl: "./medical-visit.component.html",
+  styleUrls: ["./medical-visit.component.css"]
 })
 export class MedicalVisitComponent implements OnInit {
-
-  constructor() { }
+  doctor : any;
+   
+  constructor(private http: HttpClient, private route: Router) {}
 
   ngOnInit() {
+    this.doctor = JSON.parse(localStorage.getItem("user"));
+    console.log(this.doctor)
   }
 
+  getVisits(){
+    this.http.get(environment.url + `/attentions/${this.doctor.icm}`).subscribe(data => {
+      console.log(data)
+    })
+  }
 }
