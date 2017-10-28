c00984b0dceb467627575091e50857c9e7ef3ac2
Rodrigo <rbustamantejelvez@gmail.com>
2017-10-27 22:54:25 -0300

    Added message on medical visit view

--- src/app/components/doctor/medical-visit/medical-visit.component.ts
+++ src/app/components/doctor/medical-visit/medical-visit.component.ts
@@ -4,25 +4,28 @@ import { Router } from "@angular/router";
 import * as _ from "lodash";
 import { environment } from "./../../../../environments/environment";
 
-
 @Component({
   selector: "app-medical-visit",
   templateUrl: "./medical-visit.component.html",
   styleUrls: ["./medical-visit.component.css"]
 })
 export class MedicalVisitComponent implements OnInit {
-  doctor : any;
-   
+  doctor: any;
+  evaluations: any;
+  exists: boolean;
   constructor(private http: HttpClient, private route: Router) {}
 
   ngOnInit() {
     this.doctor = JSON.parse(localStorage.getItem("user"));
-    console.log(this.doctor)
+    this.getVisits();
+    if (this.evaluations === undefined) this.exists = false;
   }
 
-  getVisits(){
-    this.http.get(environment.url + `/attentions/${this.doctor.icm}`).subscribe(data => {
-      console.log(data)
-    })
+  getVisits() {
+    this.http
+      .get(environment.url + `/attentions/${this.doctor.icm}`)
+      .subscribe(data => {
+        this.evaluations = data;
+      });
   }
 }
