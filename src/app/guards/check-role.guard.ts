4930242c2416e8aa3b8caba0d1a4418bcaf76c57
Rodrigo <rbustamantejelvez@gmail.com>
2017-10-31 12:56:49 -0300

    Fixed validation if user is doctor

--- src/app/guards/check-role.guard.ts
+++ src/app/guards/check-role.guard.ts
@@ -13,9 +13,13 @@ export class CheckRoleGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    let user = JSON.parse(localStorage.getItem("user").toString());
-    if (user.role === undefined) user.role.name === "Doctor";
     const allowedRoles = route.data["allowedRoles"];
+    let user = JSON.parse(localStorage.getItem("user").toString());
+    console.log(user);
+    if (user.role === undefined) {
+      let rol = "Doctor";
+      return allowedRoles.some(x => x === rol);
+    }
     let rol = user.role.name;
     return allowedRoles.some(x => x === rol);
   }
