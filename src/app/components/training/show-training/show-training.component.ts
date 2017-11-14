b29083112437368aeddf7beb3a001cccf30e414b
Rodrigo <rbustamantejelvez@gmail.com>
2017-11-14 20:01:12 -0300

    Added export certificate in trainings for employees

--- src/app/components/training/show-training/show-training.component.ts
+++ src/app/components/training/show-training/show-training.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from "@angular/common/http";
 import { Router, ActivatedRoute } from "@angular/router";
 import { environment } from "../../../../environments/environment";
 import * as _ from "lodash";
+import * as pdf from "jspdf";
+import * as momemt from "moment/moment";
 
 @Component({
   selector: "app-show-training",
@@ -12,7 +14,9 @@ import * as _ from "lodash";
 export class ShowTrainingComponent implements OnInit {
   training: any;
   students: any;
-  type:     any;
+  type: any;
+  user: any;
+  aux: any;
   constructor(
     private route: Router,
     private active: ActivatedRoute,
@@ -20,26 +24,62 @@ export class ShowTrainingComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem("user"));
     let id = this.active.snapshot.paramMap.get("id");
-    this.getStudents(id)
-    this.getTraining(id)
+    this.getStudents(id);
+    this.getTraining(id);
   }
 
-  getStudents(id){
+  getStudents(id) {
     this.http.get(environment.url + `/students/${id}`).subscribe(data => {
       this.students = data["students"];
-      this.students = _.map(this.students, students => {
-        students.attended = false;
-        return students;
+      this.students = _.map(this.students, stundent => {
+        if (
+          this.user.rut === stundent.employee.rut &&
+          stundent.state.state === "Asistido"
+        ) {
+          this.aux = true;
+        }
+        return stundent;
       });
-      console.log(this.students);
+      console.log(this.aux);
     });
   }
 
-  getTraining(id){
+  getTraining(id) {
     this.http.get(environment.url + `/trainings/${id}`).subscribe(data => {
-      this.training = data
+      this.training = data;
       console.log(this.training);
     });
+  }
+
+  toPDF() {
+    const doc = new pdf();
+    doc.setFontSize(15);
+    doc.text(
+      `Safe S.A. certifica que la persona con rut ${this.user.rut} y nombre`,
+      20,
+      10
+    );
+    doc.text(
+      `${this.user.firstName} ${this.user.lastName} de la empresa ${this
+        .training.customer.name},`,
+      20,
+      20
+    );
+    doc.text(
+      `asistió a la capacitación ${this.training.name} realizada el `,
+      20,
+      30
+    );
+    let mili = Number(momemt(this.training.date))
+    let date = momemt.unix(mili/1000).format("dd-MM-yyyy")
+    doc.text(
+      `${this.training.date} por el expositor ${this.training.expositor
+        .first_name} ${this.training.expositor.last_name}`,
+      20,
+      40
+    );
+    doc.save(`${this.training.name}.pdf`);
   }
 }
